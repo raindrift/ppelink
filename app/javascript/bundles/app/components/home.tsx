1fd3271009623bd605c2c8c9b7fbca88d39d07ea
Nate Burt <nate@squarerootsgrow.com>
2020-03-21 15:57:43 -0400

    remove bootstrap

--- app/javascript/bundles/app/components/home.tsx
+++ app/javascript/bundles/app/components/home.tsx
@@ -1,15 +1,14 @@
 import React, { PureComponent } from 'react';
 import { DefaultComponentState } from '../../shared/types/react';
-import Paragraph from '../../shared/components/paragraph';
 
 type Props = any;
 
 export default class Home extends PureComponent<Props, DefaultComponentState> {
   render() {
     return (
-      <Paragraph>
+      <div>
         Hello World
-      </Paragraph>
+      </div>
     );
   }
 }
